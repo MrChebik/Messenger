1a727614e1c383f048187634f5a1033d380ec542
Alexander Beschasny <mrchebik@yandex.ru>
2016-04-30 16:47:10 +0300

    Update createDB.sh

--- createDB.sh
+++ createDB.sh
@@ -1,2 +1,11 @@
 #!bin/sh
-mysql -e "CREATE DATABASE Messenger; USE Messenger; CREATE TABLE Users(id MEDIUMINT NOT NULL AUTO_INCREMENT, login VARCHAR(12) UNIQUE, password VARCHAR(16), PRIMARY KEY(id));" -u root -p
+echo "Create database 'Messenger'"
+echo -n "Username: "
+read username
+echo -n "Password: "
+read -s password
+echo
+mysql -u $username -p$password -e "
+CREATE DATABASE Messenger;
+USE Messenger;
+CREATE TABLE Users(id MEDIUMINT NOT NULL AUTO_INCREMENT, login VARCHAR(12) UNIQUE, password VARCHAR(16), PRIMARY KEY(id));"
